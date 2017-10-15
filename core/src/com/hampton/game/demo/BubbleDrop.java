5f05ac26d1c5288f337f8886b20ae4d539c85d49
Dnae-ferguson <32795626+Dnae-ferguson@users.noreply.github.com>
2017-10-15 12:50:41 -0400

    added the ability to go to the end screen

--- core/src/com/hampton/game/demo/BubbleDrop.java
+++ core/src/com/hampton/game/demo/BubbleDrop.java
@@ -154,6 +154,7 @@ public class BubbleDrop extends GameScreen {
         if (life==0 && gameOn == true) {
             gameOn=false;
             loseGame();
+            gotoScreen("EndScreen");
         }
     }
 
