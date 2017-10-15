822b46a1ed0ebe27847ce423446dbdc229437666
adrianTJenkins <adrian.jenkins99@gmail.com>
2017-10-15 12:43:43 -0400

    Revert "Moved text"
    This reverts commit 7d631330d0cad29e07277404d4e0cfd9a72be0de.

--- core/src/com/hampton/game/demo/BubbleDrop.java
+++ core/src/com/hampton/game/demo/BubbleDrop.java
@@ -150,7 +150,7 @@ public class BubbleDrop extends GameScreen {
             }
         }
 
-        scoreLabel.setText("\nScore: " + score + " Level: " + (dropSpeed - 2) + " \n Lives: " + life);
+        scoreLabel.setText("Score: " + score + " Level: " + (dropSpeed - 2) + " \n Lives: " + life);
         if (life==0 && gameOn == true) {
             gameOn=false;
             loseGame();
